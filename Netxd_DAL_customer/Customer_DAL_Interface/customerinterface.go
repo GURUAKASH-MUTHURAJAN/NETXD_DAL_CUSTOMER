51e88161335b59e7bdb618952c02f588c1184d9b
GURUAKASH-MUTHURAJAN <141702183+GURUAKASH-MUTHURAJAN@users.noreply.github.com>
2023-08-30 10:35:03 +0530

    Delete Netxd_DAL_customer directory

--- Netxd_DAL_customer/Customer_DAL_Interface/customerinterface.go
+++
@@ -1,7 +0,0 @@
-package customerinterface
-
-import customermodel "Netxd_Customer/Customer_Model"
-
-type ICustomer interface{
-	CreateCustomer(customer *customermodel.Customer)(*customermodel.CustomerResponse, error)
-}
--- Netxd_DAL_customer/go.mod
+++
@@ -1,3 +0,0 @@
-module Netxd_Customer
-
-go 1.20
